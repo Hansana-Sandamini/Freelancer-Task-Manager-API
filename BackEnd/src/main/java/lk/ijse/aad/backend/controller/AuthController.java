f647b5189f7d22b034d6bfb22b338903c61d0b46
Y. P. Hansana Sandamini <hansanasandamini2005@gmail.com>
2025-08-16 18:54:28 +0530

    Added ApplicationConfig and Initialize AuthController

---
+++ BackEnd/src/main/java/lk/ijse/aad/backend/controller/AuthController.java
@@ -0,0 +1,14 @@
+package lk.ijse.aad.backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+@CrossOrigin
+public class AuthController {
+
+}
