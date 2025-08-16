f73a3a23331e0b2d8b12e30a7b8f2c1aebf86f03
Y. P. Hansana Sandamini <hansanasandamini2005@gmail.com>
2025-08-16 16:43:23 +0530

    Added User Entity and Role Enum

---
+++ BackEnd/src/main/java/lk/ijse/aad/backend/entity/User.java
@@ -0,0 +1,26 @@
+package lk.ijse.aad.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;  // ADMIN, FREELANCER, CLIENT
+
+}
