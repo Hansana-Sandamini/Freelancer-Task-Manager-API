dce5a98b53aefa6cdef1cda89b5ea114cc9e4800
Y. P. Hansana Sandamini <hansanasandamini2005@gmail.com>
2025-08-16 17:59:12 +0530

    Added UserService Interface and AuthResponseDTO

--- BackEnd/src/main/java/lk/ijse/aad/backend/repository/UserRepository.java
+++ BackEnd/src/main/java/lk/ijse/aad/backend/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package lk.ijse.aad.backend.repository;
 
 import lk.ijse.aad.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
