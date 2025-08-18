8114d38d514e1a40c3fa36aaffcbc24f374544dd
Y. P. Hansana Sandamini <hansanasandamini2005@gmail.com>
2025-08-18 21:16:36 +0530

    Added TaskDTO and ProposalDTO Classes

---
+++ BackEnd/src/main/java/lk/ijse/aad/backend/dto/TaskDTO.java
@@ -0,0 +1,20 @@
+package lk.ijse.aad.backend.dto;
+
+import lk.ijse.aad.backend.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TaskDTO {
+    private Long id;
+    private String title;
+    private String description;
+    private String status;  // OPEN, IN_PROGRESS, COMPLETED
+    private LocalDate deadline;
+    private User client;
+}
