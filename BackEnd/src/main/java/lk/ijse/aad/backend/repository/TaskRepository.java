3343288fe38652acf0aaec0c97ceb747ad6c9c2b
Y. P. Hansana Sandamini <hansanasandamini2005@gmail.com>
2025-08-18 21:51:40 +0530

    Added TaskRepository and ProposalRepository

---
+++ BackEnd/src/main/java/lk/ijse/aad/backend/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package lk.ijse.aad.backend.repository;
+
+import lk.ijse.aad.backend.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
