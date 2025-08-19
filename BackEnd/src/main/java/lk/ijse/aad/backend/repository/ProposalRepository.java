d492181e434ef378cd63425b95dd8bdb3814829d
Y. P. Hansana Sandamini <hansanasandamini2005@gmail.com>
2025-08-19 17:57:40 +0530

    Implement methods in TaskServiceImpl and ProposalServiceImpl

--- BackEnd/src/main/java/lk/ijse/aad/backend/repository/ProposalRepository.java
+++ BackEnd/src/main/java/lk/ijse/aad/backend/repository/ProposalRepository.java
@@ -4,6 +4,12 @@ import lk.ijse.aad.backend.entity.Proposal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ProposalRepository extends JpaRepository<Proposal,Long> {
+    List<Proposal> findByTaskId(Long taskId);
+    List<Proposal> findByFreelancerId(Long freelancerId);
+    Optional<Proposal> findById(Long id);
 }
