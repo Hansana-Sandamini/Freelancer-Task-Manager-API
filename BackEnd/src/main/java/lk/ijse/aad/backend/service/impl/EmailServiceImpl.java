2af35832769953b9861ce93049ba489dd5713199
Y. P. Hansana Sandamini <hansanasandamini2005@gmail.com>
2025-08-25 18:09:23 +0530

    Implement to send a welcome email when create new account

---
+++ BackEnd/src/main/java/lk/ijse/aad/backend/service/impl/EmailServiceImpl.java
@@ -0,0 +1,36 @@
+package lk.ijse.aad.backend.service.impl;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lk.ijse.aad.backend.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class EmailServiceImpl implements EmailService {
+
+    private final JavaMailSender mailSender;
+
+    @Override
+    public void sendEmail(String to, String subject, String text) {
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(text, true);  // true = HTML content
+
+            mailSender.send(message);
+
+        } catch (MessagingException e) {
+            throw new RuntimeException("Failed to send email", e);
+        }
+    }
+
+}
