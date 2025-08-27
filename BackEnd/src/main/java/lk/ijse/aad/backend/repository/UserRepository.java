e8b37aef422e9572034b7bd3dee769536a2fcf67
Y. P. Hansana Sandamini <hansanasandamini2005@gmail.com>
2025-08-28 00:21:19 +0530

    Implement send emails to freelancers when Add new tasks

--- BackEnd/src/main/java/lk/ijse/aad/backend/repository/UserRepository.java
+++ BackEnd/src/main/java/lk/ijse/aad/backend/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package lk.ijse.aad.backend.repository;
 
+import lk.ijse.aad.backend.entity.Role;
 import lk.ijse.aad.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findByRole(Role role);
 }
